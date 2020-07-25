1d7a05ed97668069ad7dfa334845f73b891ae320
ShankarNara <shankar.sn007@gmail.com>
2020-07-25 11:51:33 +0530

    Improved UI using CSS

--- code/tango_with_django_project/rango/forms.py
+++ code/tango_with_django_project/rango/forms.py
@@ -31,13 +31,18 @@ class PageForm(forms.ModelForm):
 		return cleaned_data
 
 class UserForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput())
+	username = forms.CharField(help_text="Please enter a username : ")
+	email = forms.CharField(help_text="Please enter email : ")
+	password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password : ")
 
 	class Meta:
 		model=User
-		fields=('username','email','password')
+		fields=['username','email','password']
 
 class UserProfileForm(forms.ModelForm):
+	website = forms.URLField(help_text="Please enter ypur website.", required = False)
+	picture = forms.ImageField("Select an Image to upload.")	
+
 	class Meta:
 		model = UserProfile	
-		fields=('website','picture')	
+		fields=['website','picture']	
